d3a9c6e74ce6b0c9968bc4523b6069881dacab4e
KrzysztofLeczycki <92303895+KrzysztofLeczycki@users.noreply.github.com>
2022-01-01 22:13:19 +0100

    Update Arabic to Roman Converter.js

--- fcc certification projects/Arabic to Roman Converter.js
+++ fcc certification projects/Arabic to Roman Converter.js
@@ -1,5 +1,3 @@
-//Moje rozwiązanie
-
 function convertToRoman(num) {
     const conArr = 
     [{order: 'unity', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6:'VI', 7: 'VII', 8: 'VIII', 9: 'IX'},
@@ -9,36 +7,15 @@ function convertToRoman(num) {
     
     let numStr = num.toString();
     const len = numStr.length;
-   let result = '';
-   if (num <= 0 || num > 3999){
-     result = 'Out of range';
-   } else {
-     for (let i = len - 1; i >= 0; i--){
-       if (numStr[i] !== '0'){
-         result = conArr[len - i -1][numStr[i]] + result;
+    let result = '';
+    if (num <= 0 || num > 3999){
+      result = 'Out of range';
+    } else {
+      for (let i = len - 1; i >= 0; i--){
+        if (numStr[i] !== '0'){
+          result = conArr[len - i -1][numStr[i]] + result;
        }
      }
    }
    return result;
    }
-   
-   
-   console.log(convertToRoman(37));
-
-   //Na podstawie StackOverflow
-   let romanMatrix = [
-    [1000, 'M'],
-    [900, 'CM'],
-    [500, 'D'],
-    [400, 'CD'],
-    [100, 'C'],
-    [90, 'XC'],
-    [50, 'L'],
-    [40, 'XL'],
-    [10, 'X'],
-    [9, 'IX'],
-    [5, 'V'],
-    [4, 'IV'],
-    [1, 'I']
-  ];
-  
